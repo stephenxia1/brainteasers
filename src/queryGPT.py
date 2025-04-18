ce38a96d0948a46848262d64a46a1668bb3ff1a8
stephenxia1 <149318713+stephenxia1@users.noreply.github.com>
2025-04-18 19:36:24 -0400

    fixed o1 prompting

--- src/queryGPT.py
+++ src/queryGPT.py
@@ -18,16 +18,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", help="Experiment Name", default=f'TestExperiment-{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}', required=True)
     parser.add_argument("--dataset", help="Dataset to run on", choices=["Math", "Logic"], default="Math", required=True)
+    parser.add_argument("--prompt", help="Prompting File to use", required=True)
     parser.add_argument("--rows", help="Number of rows to sample", type=int, default=1, required=False)
     parser.add_argument("--samples", help="Number of samples to run", type=int, default=1, required=False)
     
     args = parser.parse_args()
-    os.makedirs(f"../../results/{args.name}", exist_ok=True)
+    os.makedirs(f"../responses/{args.dataset}/{args.name}", exist_ok=True)
 
     data = pd.read_csv(f'../data/braingle/braingle_{args.dataset}.csv')
     outputs = pd.DataFrame(columns=['Question', 'Response', 'Correct', 'BruteForce'])
 
-    instructions = readPrompt(f'../../prompting/{args.name}.txt')
+    instructions = readPrompt(f'../prompting/{args.prompt}.txt')
     
     client = OpenAI(
         api_key= os.getenv("OPENAI_API_KEY")
@@ -43,51 +44,53 @@ def main():
             hint = row['Hint']
 
             response = client.responses.create(
-                model="gpt-4o",
-                input=[
-                    {"role": "system", "content": instructions},
-                    {"role": "user", "content": question}
-                ],
-                text={
-                    "format": {
-                        "type": "json_schema",
-                        "name": "math_reasoning",
-                        "schema": {
-                            "type": "object",
-                            "properties": {
-                                "steps": {
-                                    "type": "array",
-                                    "items": {
-                                        "type": "object",
-                                        "properties": {
-                                            "explanation": { "type": "string" },
-                                            "output": { "type": "string" }
-                                        },
-                                        "required": ["explanation", "output"],
-                                        "additionalProperties": False
-                                    }
-                                },
-                                "final_answer": { "type": "string" }
-                            },
-                            "required": ["steps", "final_answer"],
-                            "additionalProperties": False
-                        },
-                        "strict": True
-                    }
-                }
+                # model="gpt-o1-2024-12-17",
+                model="o1-2024-12-17",
+                instructions=instructions,
+                input=question,
+                # text={
+                #     "format": {
+                #         "type": "json_schema",
+                #         "name": "math_reasoning",
+                #         "schema": {
+                #             "type": "object",
+                #             "properties": {
+                #                 "steps": {
+                #                     "type": "array",
+                #                     "items": {
+                #                         "type": "object",
+                #                         "properties": {
+                #                             "explanation": { "type": "string" },
+                #                             "output": { "type": "string" }
+                #                         },
+                #                         "required": ["explanation", "output"],
+                #                         "additionalProperties": False
+                #                     }
+                #                 },
+                #                 "final_answer": { "type": "string" }
+                #             },
+                #             "required": ["steps", "final_answer"],
+                #             "additionalProperties": False
+                #         },
+                #         "strict": True
+                #     }
+                # }
+                max_output_tokens=100000,
             )
             print(response.output_text)
 
             # modelResponse = response.output[0].content[0]
+            modelResponse = response.output_text
 
             outputs = pd.concat([outputs, pd.DataFrame({
                 'Question': [question],
-                'Response': [response.output_text],
-                'Correct': [solution],
-                'BruteForce': [hint]
+                'Response': [modelResponse],
+                'Correct': [None],
+                'BruteForce': [None],
+                'Status': [response.status],
             })], ignore_index=True)
 
-    outputs.to_csv(f"../results/{args.name}-results.csv", index=False)
+    outputs.to_csv(f"../responses/{args.dataset}/{args.name}/results.csv", index=False)
 
 if __name__ == "__main__":
     main()
