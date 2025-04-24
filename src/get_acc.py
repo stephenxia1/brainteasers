9d1da5bc92b73e5ae087cf28afb870d5c407ecd6
Simeng Han <shirleyhsm6@gmail.com>
2025-04-24 16:46:00 -0400

    Update get_acc.py

--- src/get_acc.py
+++ src/get_acc.py
@@ -1,5 +1,67 @@
 import pandas as pd
 import argparse, json
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+def get_token_num(text):
+    tokens = word_tokenize(text)
+    return len(tokens)
+
+def get_acc(args):
+
+    total_len_dict = {
+        "basicprompt": 0,
+        "mathPrompt": 0,
+        "implicit_symbol_reasoning_prompt": 0,
+        "explicit_symbol_reasoning_prompt": 0,
+        "hint_prompt": 0,
+    }
+        
+    correct_count_dict = {
+        "basicprompt": [0, 0, 0, 0, 0],
+        "mathPrompt": [0, 0, 0, 0, 0],
+        "implicit_symbol_reasoning_prompt": [0, 0, 0, 0, 0],
+        "explicit_symbol_reasoning_prompt": [0, 0, 0, 0, 0],
+        "hint_prompt": [0, 0, 0, 0, 0],
+    }
+
+    token_num_dict = {
+        "basicprompt": [0, 0, 0, 0, 0],
+        "mathPrompt": [0, 0, 0, 0, 0],
+        "implicit_symbol_reasoning_prompt": [0, 0, 0, 0, 0],
+        "explicit_symbol_reasoning_prompt": [0, 0, 0, 0, 0],
+        "hint_prompt": [0, 0, 0, 0, 0],
+    }
+
+    with open(f"../response_evaluation/{args.dataset}/{args.name}/resultsEvaluations.jsonl") as f:
+        for line in f:
+            data = json.loads(line)
+
+            prompttype = data["PromptType"]
+            total_len_dict[prompttype] += 1
+            if data['correctness'] == "1":
+                correct_count_dict[prompttype][0] += 1
+                token_num_dict[prompttype][0] += get_token_num(data['Response'])
+                if total_len_dict[prompttype] <= 50:
+                    correct_count_dict[prompttype][1] += 1
+                    token_num_dict[prompttype][1] += get_token_num(data['Response'])
+                if total_len_dict[prompttype] > 25 and total_len_dict[prompttype] <= 50:
+                    correct_count_dict[prompttype][2] += 1
+                    token_num_dict[prompttype][2] += get_token_num(data['Response'])
+                if total_len_dict[prompttype] <= 25:
+                    correct_count_dict[prompttype][3] += 1
+                    token_num_dict[prompttype][3] += get_token_num(data['Response'])
+                if total_len_dict[prompttype] >50:
+                    correct_count_dict[prompttype][4] += 1
+                    token_num_dict[prompttype][4] += get_token_num(data['Response'])
+            
+
+        acc_dict = {k: [v[0]/250, v[1]/50, v[2]/25, v[3]/25, v[4]/200] for k, v in correct_count_dict.items()} # still need to handle missing items
+        token_dict = {k: [v[0]/250, v[1]/50, v[2]/25, v[3]/25, v[4]/200] for k, v in token_num_dict.items()}
+        print(acc_dict)
+        print(token_dict)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,31 +70,8 @@ def main():
     
     args = parser.parse_args()
 
-    correct_count_dict = {
-        "basicprompt": [0, 0, 0, 0],
-        "mathPrompt": [0, 0, 0, 0],
-        "implicit_symbol_reasoning_prompt": [0, 0, 0, 0],
-        # "explicit_symbol_reasoning_prompt": [0, 0, 0, 0],
-        "hint_prompt": [0, 0, 0, 0],
-    }
+    get_acc(args)
 
-    with open(f"../response_evaluation/{args.dataset}/{args.name}/resultsEvaluations.jsonl") as f:
-        total_len = 0
-        for line in f:
-            data = json.loads(line)
-
-            if data['correctness'] == "1":
-                correct_count_dict[data["PromptType"]][0] += 1
-                if total_len <= 50:
-                    correct_count_dict[data["PromptType"]][1] += 1
-                if total_len >= 25 and total_len <= 50:
-                    correct_count_dict[data["PromptType"]][2] += 1
-                if total_len <= 25:
-                    correct_count_dict[data["PromptType"]][3] += 1
-            total_len += 1
-
-        acc_dict = {k: [v[0]/250, v[1]/50, v[2]/25, v[3]/25] for k, v in correct_count_dict.items()} # still need to handle missing items
-        print(acc_dict)
     
 if __name__ == "__main__":
     main()
